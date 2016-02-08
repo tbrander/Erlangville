e1c703fc5a02cb08e53b7ef4d51954c26b502fc2
Thomas <brandertb@gmail.com>
2016-02-08 14:29:04 +0100

    3.1 done

---
+++ src/erlangClient.erl
@@ -0,0 +1,22 @@
+-module(erlangClient).
+
+-export([start_link/2, stop/1]).
+-export([release_cycle/1, secure_cycle/1, get_info/1]).
+
+
+
+stop(Ref) -> gen_fsm:sync_send_event(Ref, stop). 
+
+start_link(Total, Occupied) ->
+	{ok, Ref} =erlangvilleServer:start_link(Total, Occupied).
+	
+
+release_cycle(Ref) ->
+	gen_fsm:sync_send_event(Ref, release).
+
+
+secure_cycle(Ref) ->
+	gen_fsm:sync_send_event(Ref, secure).
+
+get_info(Ref) ->
+	gen_fsm:sync_send_all_state_event(Ref, info).
---
+++ src/erlangvilleServer.erl
@@ -0,0 +1,59 @@
+-module(erlangvilleServer).
+-behaviour(gen_fsm).
+
+
+-export([start_link/2, init/1, terminate/3, handle_sync_event/4]).
+-export([empty/3, full/3, idle/3]).
+
+
+start_link(Total, Occupied) ->
+ gen_fsm:start_link({local, ?MODULE}, ?MODULE, {Total, Occupied}, []). 
+
+init({Total, Occupied}) ->
+	{ok, idle, {Total, Occupied}}.
+
+%% Terminate
+terminate(Reason, StateName, LoopData) -> ok.
+
+
+%% IDLE STATE
+idle(release, From, {Total, Occupied}) when Occupied==1 ->
+	{reply, ok, empty, {Total, Occupied-1}};
+idle(release, From, {Total, Occupied}) when Occupied > 1 ->
+	{reply, ok, idle, {Total, Occupied-1}};
+idle(secure, From, {Total, Occupied}) when Occupied == (Total-1) ->
+	{reply, ok, full, {Total, Occupied+1}};
+idle(secure, From, {Total, Occupied}) when Occupied < (Total-1) ->
+	{reply, ok, idle, {Total, Occupied+1}};
+idle(info, From, {Total, Occupied}) ->
+	{reply, getInfo(Total, Occupied), idle, {Total, Occupied}}.
+
+%%EMPTY STATE
+
+handle_sync_event(info,From, StateName, {Total, Occupied}) ->
+    {reply, getInfo(Total, Occupied), StateName, {Total, Occupied}}.
+
+empty(secure, From, {Total, Occupied}) ->
+	{reply, ok, idle, {Total, Occupied+1}};
+empty(info, From, {Total, Occupied}) ->
+	{reply, getInfo(Total, Occupied), empty, {Total, Occupied}};
+empty(_Other, From, {Total, Occupied})->
+	{reply, {error, empty}, empty, {Total, Occupied}}.
+
+
+%% FULL STATE 
+full(release, From, {Total, Occupied}) ->
+	{reply, ok, idle, {Total, Occupied-1}};
+full(info, From, {Total, Occupied}) ->
+	{reply, getInfo(Total, Occupied), full, {Total, Occupied}};
+full(_Other, From, {Total, Occupied})->
+	{reply, {error, full}, full, {Total, Occupied}}.
+
+
+
+getInfo(Total, Occupied) ->
+	{ok, [{total, Total}, {occupied, Occupied}, {free, (Total-Occupied)}]}.
+
+%% Occupied=0 betyder tom docka
+		
+	
